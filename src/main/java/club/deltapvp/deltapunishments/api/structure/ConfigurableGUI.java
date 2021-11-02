4f36a3338a128bf18e211838b970a1798d40f7b1
Negative <60104846+NegativeKB@users.noreply.github.com>
2021-11-02 13:37:42 -0230

    Added configurable filler information

--- src/main/java/club/deltapvp/deltapunishments/api/structure/ConfigurableGUI.java
+++ src/main/java/club/deltapvp/deltapunishments/api/structure/ConfigurableGUI.java
@@ -1,11 +1,13 @@
 package club.deltapvp.deltapunishments.api.structure;
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 import java.util.Map;
 
 @Data
+@RequiredArgsConstructor
 public class ConfigurableGUI {
 
     private final String id;
@@ -14,14 +16,7 @@ public class ConfigurableGUI {
     private final List<Integer> fillerSlots;
     private final boolean enabled;
     private final Map<String, Map<String, Object>> slots;
-
-    public ConfigurableGUI(String id, String title, int rows, List<Integer> fillerSlots, boolean enabled, Map<String, Map<String, Object>> slots) {
-        this.id = id;
-        this.title = title;
-        this.rows = rows;
-        this.fillerSlots = fillerSlots;
-        this.slots = slots;
-        this.enabled = enabled;
-    }
+    private String fillerMaterial;
+    private int fillerData;
 
 }
